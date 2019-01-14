3c906e622e07f738cccaa114ad80a71abd1b91cd
Anthony <anthonydevenuto@gmail.com>
2019-01-13 19:18:12 -0600

    Updates

---
+++ precache-manifest.eae14376a4e45213ef819a4d61c79577.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1db9c008de062ace1042",
+    "url": "/react_youtube_clone/static/js/runtime~main.1db9c008.js"
+  },
+  {
+    "revision": "28eaad2ec9f4a6c869e5",
+    "url": "/react_youtube_clone/static/js/main.28eaad2e.chunk.js"
+  },
+  {
+    "revision": "d134149590c25c27acff",
+    "url": "/react_youtube_clone/static/js/1.d1341495.chunk.js"
+  },
+  {
+    "revision": "28eaad2ec9f4a6c869e5",
+    "url": "/react_youtube_clone/static/css/main.e2bf6bda.chunk.css"
+  },
+  {
+    "revision": "d134149590c25c27acff",
+    "url": "/react_youtube_clone/static/css/1.83052501.chunk.css"
+  },
+  {
+    "revision": "765f07ac1fea3b9a09ccf45c7929d569",
+    "url": "/react_youtube_clone/index.html"
+  }
+];
--- src/config/keys.js
+++
@@ -1,4 +0,0 @@
-const keys = {
-    api_key: 'AIzaSyBsv0M9DxPzlUHJh3QiDXJvuvVwePYa8R0'
-}
-export default keys;
